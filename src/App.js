4fe5440ac47409568ef58a2a468a4ec97af96f69
Joscelyn James <josce.james7@hotmail.com>
2018-01-10 22:11:29 -0700

    added axios get request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://g-blockchain-info-api.herokuapp.com/rawaddr/1AaT3EeMUPjQyw5orzi929SHfXQCehYZGp')
+      .then((res) => {
+        console.log(res);
+      })
+  }
+
   render() {
     return (
       <div className="App">
