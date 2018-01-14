4ae4d7cdf7448b201ebf0ff101823289fb4f7609
Joscelyn James <josce.james7@hotmail.com>
2018-01-14 09:56:12 -0700

    added redux and getting txs data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore , applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from './store/reducer';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
